57f507e22537dfb7b35815e4ce782cd84bb75001
maxM <mw159159@gmail.com>
2020-11-27 14:50:44 +0500

    _

--- http.sh
+++ http.sh
@@ -3,7 +3,7 @@ do
 	echo "$x" 1>&2
 done
 
-echo "HTTP/1.1 $RANDOM OK"
+echo "HTTP/1.1 200 OK"
 echo "Date: Wed, 11 Feb 2009 11:20:59 GMT"
 echo "Server: Apache"
 echo "X-Powered-By: PHP/5.2.4-2ubuntu5wm1"
